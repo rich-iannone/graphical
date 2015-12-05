50e808db210df7615b28a121dbae38687dacbb53
Richard Iannone <riannone@me.com>
2015-12-05 09:46:51 -0800

    Modify roxygen @param statements

--- R/connect_nodes.R
+++ R/connect_nodes.R
@@ -3,12 +3,13 @@
 #' space.
 #' @param graph a graph object of class \code{dgr_graph} that is created using
 #' \code{create_graph}.
-#' @param from
-#' @param to
+#' @param from the node from which a connection will initiate.
+#' @param to the node to which a connection will terminate.
 #' @return a graph object of class \code{dgr_graph}.
 #' @export connect_nodes
 
 connect_nodes <- function(graph, from, to){
   
+  add_edge()
   
 }
