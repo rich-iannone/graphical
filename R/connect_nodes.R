a0fe8e62be549c144e16dfd3c828299395171a0e
Richard Iannone <riannone@me.com>
2015-12-05 09:45:24 -0800

    Add function 'connect_nodes'

---
+++ R/connect_nodes.R
@@ -0,0 +1,14 @@
+#' Create a connection between nodes
+#' @description Create a connection between two different nodes in the graph
+#' space.
+#' @param graph a graph object of class \code{dgr_graph} that is created using
+#' \code{create_graph}.
+#' @param from
+#' @param to
+#' @return a graph object of class \code{dgr_graph}.
+#' @export connect_nodes
+
+connect_nodes <- function(graph, from, to){
+  
+  
+}
